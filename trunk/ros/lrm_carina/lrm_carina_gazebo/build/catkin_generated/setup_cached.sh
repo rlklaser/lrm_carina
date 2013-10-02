522473b5bc9040a526b876e5545d5454ea38e148
rlklaser <rlklaser@8ec145bb-1b84-47e7-9d5d-b822e72310b9>
2013-10-02 16:54:33 +0000

    bin

---
+++ trunk/ros/lrm_carina/lrm_carina_gazebo/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+export CATKIN_TEST_RESULTS_DIR="/home/rlklaser/Work/ros/sandbox/lrm_carina_gazebo/build/test_results"
+export ROS_TEST_RESULTS_DIR="/home/rlklaser/Work/ros/sandbox/lrm_carina_gazebo/build/test_results"
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/rlklaser/Work/ros/sandbox/lrm_carina_gazebo/build/devel:$CMAKE_PREFIX_PATH"
+export CPATH="/home/rlklaser/Work/ros/sandbox/lrm_carina_gazebo/build/devel/include:$CPATH"
+export LD_LIBRARY_PATH="/home/rlklaser/Work/ros/sandbox/lrm_carina_gazebo/build/devel/lib:$LD_LIBRARY_PATH"
+export PATH="/home/rlklaser/Work/ros/sandbox/lrm_carina_gazebo/build/devel/bin:$PATH"
+export PKG_CONFIG_PATH="/home/rlklaser/Work/ros/sandbox/lrm_carina_gazebo/build/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
+export PYTHONPATH="/home/rlklaser/Work/ros/sandbox/lrm_carina_gazebo/build/devel/lib/python2.7/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/rlklaser/Work/ros/sandbox/lrm_carina_gazebo/build/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/rlklaser/Work/ros/sandbox/lrm_carina_gazebo:/home/rlklaser/Work/ros/pcl_ws/install_isolated/share:/home/rlklaser/Work/ros/pcl_ws/install_isolated/stacks:/opt/ros/groovy/share:/opt/ros/groovy/stacks"
